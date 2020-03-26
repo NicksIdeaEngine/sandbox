754777dbaed8eb412dcd6aede87bf0c0b1821c07
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-03-26 11:45:33 -0400

    Post restore commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+<<<<<<< HEAD
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
+=======
+/* eslint-disable react/no-unused-state */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/destructuring-assignment */
+>>>>>>> 4442f8b... Post restore commit
 import React, { Component } from 'react';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+<<<<<<< HEAD
       logged: true,
     };
   }
@@ -21,6 +30,32 @@ class App extends Component {
     return (
       <div>
         <p>{wordDisplay}</p>
+=======
+      loading: false,
+      character: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch('https://swapi.co/api/people/1')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          loading: false,
+          character: data,
+        });
+      });
+  }
+
+  render() {
+    const text = this.state.loading ? 'Loading...' : this.state.character.name;
+    return (
+      <div>
+        <p>Here&apos;s some text!</p>
+        <p>{text}</p>
+        <p>more text</p>
+>>>>>>> 4442f8b... Post restore commit
       </div>
     );
   }
