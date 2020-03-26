b5e55d9c0170a49c91b290b721f9190ed2d3bccf
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-03-26 13:26:46 -0400

    Swapi variation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-<<<<<<< HEAD
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prefer-stateless-function */
-=======
 /* eslint-disable react/no-unused-state */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-console */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/destructuring-assignment */
->>>>>>> 4442f8b... Post restore commit
 import React, { Component } from 'react';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-<<<<<<< HEAD
-      logged: true,
-    };
-  }
-
-  render() {
-    let wordDisplay;
-    if (this.state.logged) {
-      wordDisplay = 'in';
-    } else {
-      wordDisplay = 'out';
-    }
-
-    return (
-      <div>
-        <p>{wordDisplay}</p>
-=======
       loading: false,
       character: {},
     };
@@ -55,7 +33,6 @@ class App extends Component {
         <p>Here&apos;s some text!</p>
         <p>{text}</p>
         <p>more text</p>
->>>>>>> 4442f8b... Post restore commit
       </div>
     );
   }
